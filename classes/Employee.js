69313264b04df6a9b522463d56f4e3d15dc6a171
Codey Gallup <codey.gallup@gmail.com>
2024-10-30 10:37:16 -0600

    Passing all bases tests

--- classes/Employee.js
+++ classes/Employee.js
@@ -1,10 +1,44 @@
 class Employee {
+  #salary;
+  #isHired;
+  static #allEmployees = [];
+  constructor(name, position, salary) {
+    this.name = name;
+    this.position = position;
+    this.#salary = salary;
+    this.#isHired = true;
+    Employee.#allEmployees.push(this);
+  }
 
+  getSalary() {
+    return this.#salary;
+  }
 
+  setSalary(amount) {
+    this.#salary = amount;
+  }
 
-    
+  getStatus() {
+    return this.#isHired;
+  }
+
+  setStatus(command) {
+    this.#isHired = command === "fire" ? false : true;
+  }
+
+  static getEmployees() {
+    return Employee.#allEmployees;
+  }
+
+  static getTotalPayroll() {
+    let sum = 0;
+    for (let employee of Employee.#allEmployees) {
+      sum += employee.getSalary();
+    }
+    return sum;
+  }
 }
 
 module.exports = {
-    Employee,
-}
+  Employee,
+};
--- classes/Manager.js
+++ classes/Manager.js
@@ -1,10 +1,22 @@
-class Manager {
+const { Employee } = require("./Employee");
 
+class Manager extends Employee {
+  #employeesManaged;
+  constructor(name, position, salary, department) {
+    super(name, position, salary);
+    this.department = department;
+    this.#employeesManaged = [];
+  }
 
+  getEmployeesManaged() {
+    return this.#employeesManaged;
+  }
 
-    
+  setEmployeesManaged(employee) {
+    this.#employeesManaged.push(employee);
+  }
 }
 
 module.exports = {
-    Manager,
-}
+  Manager,
+};
